1e8af4aee7e31a68971ce1999ca86af202611eb2
Farzaneh Hadadi <farzahadadi@gmai.com>
2021-08-24 16:17:03 +0430

    template for projects using next.js material-ui and typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next//head";
+import { Button } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "../styles/theme";
+import CssBaseline from "@material-ui/core/CssBaseline";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
